93e7a1676014a9fd33cc90e7e7cb99fd7ff4d8b3
xalfonso <eddie.alfonso@gmail.com>
2016-10-07 02:09:31 +0900

    ...

--- www/js/Admin/listUserController.js
+++ www/js/Admin/listUserController.js
@@ -1,4 +1,18 @@
-adminModule.controller("listUserController", function($scope) {
-    $scope.firstName    = "John";
-    $scope.lastName= "Doe";
-});
+ //document.getElementById("spantest").innerHTML = "Eduardo Alfonso";
+alert("leydisss");
+ons.bootstrap();
+/*adminModule.controller("listUserController", function($scope) {
+   
+$scope.valor = "Eduardo Alfonso SAnchez";  
+   // console.log("dsfsdfsfsdfsdfs");
+    
+    $http({
+        method : "GET",
+        url : "http://181.175.221.140:8080/search/user/"
+    }).then(function mySucces(response) {
+        $scope.users = response.data.result;
+    }, function myError(response) {
+        $scope.msg = response.data.message.message;
+    });
+    
+});*/
--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,3 @@
 // This is a JavaScript file
-
+alert("eduardo");
 ons.bootstrap();
